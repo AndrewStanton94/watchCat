9edf6a3931936ac70756af2f3c3307016b9971bf
Andrew Stanton <up683693@myport.ac.uk>
2021-04-02 22:34:13 +0100

    Setting up redux for the video list

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import videoSlice from '../features/videoContainer/videoSlice';
 
 export default configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+	reducer: {
+		counter: counterReducer,
+		videos: videoSlice,
+	},
 });
---
+++ src/features/videoContainer/videoSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const videoSlice = createSlice({
+	name: 'videos',
+	initialState: {
+		videos: [],
+	},
+	reducers: {
+		setVideoList: (state, action) => {
+			state.videos = action.payload;
+		},
+		addVideos: (state, action) => {
+			state.videos = [...state.videos, ...action.payload];
+		},
+	},
+});
+
+export const { setVideoList, addVideos } = videoSlice.actions;
+
+export const getVideos = (state) => state.videos.videos;
+export const getVideoByName = (state, title) =>
+	state.videos.videos.find(({ video }) => video.title === title);
+
+export default videoSlice.reducer;
