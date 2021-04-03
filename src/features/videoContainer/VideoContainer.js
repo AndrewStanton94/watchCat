dde9c411c2aca5ac772a4489a5b9e596c5f5da6d
Andrew Stanton <up683693@myport.ac.uk>
2021-04-03 01:08:38 +0100

    Passing video list via redux

--- src/features/videoContainer/VideoContainer.js
+++ src/features/videoContainer/VideoContainer.js
@@ -1,47 +1,51 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
 import VideoList from '../videoList/VideoList';
 import ChannelContainer from '../channelContainer/ChannelContainer';
 
-export default class VideoContainer extends React.Component {
-	processInput(videoData) {
-		if (!videoData) {
-			return undefined;
-		}
-		const initialValue = {
-			channels: {},
-			videos: [],
-		};
-		const processedData = videoData.reduce((acc, current) => {
-			const { channels } = acc;
-			const channelNames = Object.keys(channels);
-			const { video, channel } = current;
-			const { title } = video;
-			const { name } = channel;
-			if (channelNames.includes(name)) {
-				channels[name].push(title);
-			} else {
-				channels[name] = [title];
-			}
+import { setVideoList } from './videoSlice';
 
-			acc.channels = channels;
-			acc.videos.push(current);
-			return acc;
-		}, initialValue);
-
-		return processedData;
+const processInput = (videoData) => {
+	if (!videoData) {
+		return undefined;
 	}
-
-	render() {
-		const processedInputs = this.processInput(this.props.videos);
-		if (!processedInputs) {
-			return null;
+	const initialValue = {
+		channels: {},
+		videos: [],
+	};
+	const processedData = videoData.reduce((acc, current) => {
+		const { channels } = acc;
+		const channelNames = Object.keys(channels);
+		const { video, channel } = current;
+		const { title } = video;
+		const { name } = channel;
+		if (channelNames.includes(name)) {
+			channels[name].push(title);
+		} else {
+			channels[name] = [title];
 		}
-		const { videos, channels } = processedInputs;
-		return (
-			<>
-				<VideoList videos={videos} />
-				<ChannelContainer channels={channels} />
-			</>
-		);
+
+		acc.channels = channels;
+		acc.videos.push(current);
+		return acc;
+	}, initialValue);
+
+	return processedData;
+};
+
+export default function VideoContainer(props) {
+	const dispatch = useDispatch();
+	const processedInputs = processInput(props.videos);
+	if (!processedInputs) {
+		return null;
 	}
+	const { videos, channels } = processedInputs;
+	dispatch(setVideoList(videos));
+	return (
+		<>
+			<VideoList />
+			<ChannelContainer channels={channels} />
+		</>
+	);
 }
--- src/features/videoList/VideoList.js
+++ src/features/videoList/VideoList.js
@@ -1,29 +1,32 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 import UtilTable from '../utilComponents/UtilTable';
 import VideoLink from '../videoLink/VideoLink';
 import VideoDuration from '../videoDuration/VideoDuration';
 import ChannelLink from '../channelLink/ChannelLink';
 
-export default class VideoList extends React.Component {
-	prepareData(videos) {
-		return videos.map(({ video, channel }) => {
-			return [
-				<VideoLink video={video} />,
-				<VideoDuration video={video} />,
-				<ChannelLink channel={channel} />,
-			];
-		});
-	}
+import { getVideos } from '../videoContainer/videoSlice';
 
-	render() {
-		return (
-			<section>
-				<h2>Your Videos</h2>
-				<UtilTable
-					headers={['Video', 'Duration', 'Channel']}
-					data={this.prepareData(this.props.videos)}
-				/>
-			</section>
-		);
-	}
+const prepareData = (videos) => {
+	return videos.map(({ video, channel }) => {
+		return [
+			<VideoLink video={video} />,
+			<VideoDuration video={video} />,
+			<ChannelLink channel={channel} />,
+		];
+	});
+};
+
+export default function VideoList() {
+	const videos = useSelector(getVideos);
+	return (
+		<section>
+			<h2>Your Videos</h2>
+			<UtilTable
+				headers={['Video', 'Duration', 'Channel']}
+				data={prepareData(videos)}
+			/>
+		</section>
+	);
 }
