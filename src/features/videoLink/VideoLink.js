9528755dad4cec4e3924e826ebbe7e4f307c3a8e
Andrew Stanton <up683693@myport.ac.uk>
2021-04-05 23:09:14 +0100

    Added a Video lookup component
    - It uses to redux to search by video title
        - Then passes the data to the VideoLink component
    - Also passes any children elements appending after the link

--- src/features/videoLink/VideoLink.js
+++ src/features/videoLink/VideoLink.js
@@ -1,12 +1,23 @@
+import { useSelector } from 'react-redux';
+
+import { getVideoByName } from '../videoContainer/videoSlice';
+
 function generateLink(id) {
 	return `https://www.youtube.com/watch?v=${id}`;
+}
+
+export function GetVideo(params) {
+	const { video } = useSelector((state) =>
+		getVideoByName(state, params.videoTitle)
+	);
+	return <VideoLink children={params.children} video={video} />;
 }
 
 export default function VideoLink(props) {
 	const { title, id } = props.video;
 	return (
 		<div>
-			<a href={generateLink(id)}>{title}</a>
+			<a href={generateLink(id)}>{title}</a> {props.children}
 		</div>
 	);
 }
