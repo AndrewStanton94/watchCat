5e31c12bcf816531daafaa1c6c7798b6776f8255
Andrew Stanton <up683693@myport.ac.uk>
2021-04-06 00:28:15 +0100

    Improved presentation of videos by channels

--- src/features/channelContainer/ChannelContainer.js
+++ src/features/channelContainer/ChannelContainer.js
@@ -1,27 +1,53 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+
+import { GetVideo } from '../videoLink/VideoLink';
+
 import { getOneVideoChannels, getMultiVideoChannels } from './channelSlice';
 
-const listRender = (list = [], show = false) => {
-	if (list) {
-		return list.map((item, i) => <p key={i}>{show ? item[show] : item}</p>);
-	}
-};
-
 export default function ChannelContainer() {
-	const oneVideoChannels = useSelector(getOneVideoChannels);
-	const multiVideoChannels = useSelector(getMultiVideoChannels);
+	const oneVideoChannels = (
+		<ul>
+			{useSelector(getOneVideoChannels).map(
+				([channelName, videoTitle], i) => (
+					<li>
+						<GetVideo videoTitle={videoTitle[0]} key={i}>
+							<span>by {channelName}</span>
+						</GetVideo>
+					</li>
+				)
+			)}
+		</ul>
+	);
+	const multiVideoChannels = useSelector(getMultiVideoChannels).map(
+		([channel, videos]) => {
+			return (
+				<section>
+					<h4>{channel}</h4>
+					<ul>
+						{videos.map((video) => (
+							<li>
+								<GetVideo videoTitle={video} />
+							</li>
+						))}
+					</ul>
+				</section>
+			);
+		}
+	);
+	console.log(multiVideoChannels);
+
 	return (
 		<section>
 			<h2>Your Channels</h2>
 			<div className="flex flex-equal gap-5r">
 				<div>
 					<h3>Channels with only one video</h3>
-					<div>{listRender(oneVideoChannels, '0')}</div>
+					{oneVideoChannels}
 				</div>
 				<div>
 					<h3>Channels with multiple videos</h3>
-					<div>{listRender(multiVideoChannels, '0')}</div>
+					{multiVideoChannels}
 				</div>
 			</div>
 		</section>
