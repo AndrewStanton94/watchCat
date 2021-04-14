bdc4370b7d5b480d692b7f88e83de0fa7545d36f
Andrew Stanton <up683693@myport.ac.uk>
2021-04-14 23:11:06 +0100

    Option to show the duration in VideoLink

--- src/features/videoLink/VideoLink.js
+++ src/features/videoLink/VideoLink.js
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 
 import { getVideoByName } from '../videoContainer/videoSlice';
+import VideoDuration from '../videoDuration/VideoDuration';
 
 function generateLink(id) {
 	return `https://www.youtube.com/watch?v=${id}`;
@@ -10,14 +11,23 @@ export function GetVideo(props) {
 	const { video } = useSelector((state) =>
 		getVideoByName(state, props.videoTitle)
 	);
-	return <VideoLink children={props.children} video={video} />;
+	return (
+		<VideoLink
+			children={props.children}
+			video={video}
+			showDuration={props.showDuration}
+		/>
+	);
 }
 
 export default function VideoLink(props) {
 	const { title, id } = props.video;
+	const { showDuration } = props;
 	return (
 		<div>
-			<a href={generateLink(id)}>{title}</a> {props.children}
+			{showDuration && <VideoDuration video={props.video} /> }
+			<a href={generateLink(id)}>{title}</a>
+			{props.children}
 		</div>
 	);
 }
