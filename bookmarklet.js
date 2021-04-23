f05eba822be0bffe6deb9a5304f9108e2ca3fb15
Andrew Stanton <up683693@myport.ac.uk>
2021-04-23 02:01:29 +0100

    Updates

--- bookmarklet.js
+++ bookmarklet.js
@@ -84,7 +84,7 @@
 	const payload = JSON.stringify(videoData.slice(0, 174));
 	const remainingVideos = JSON.stringify(videoData.slice(174));
 	const baseURL = 'https://andrewstanton94.github.io/watchCat/';
-	// const baseURL = 'http://localhost:3000/';
+	// const baseURL = 'http://localhost:3000/watchCat/';
 	const redirectURL = [baseURL, '#', payload].join('');
 	window.open(redirectURL, '_blank');
 
