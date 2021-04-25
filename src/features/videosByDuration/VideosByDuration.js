92c871b028cbb98a211fa0569ca7a98c444d0c07
Andrew Stanton <up683693@myport.ac.uk>
2021-04-25 02:06:18 +0100

    Adding id to correct element
    - The headers have the `scroll-margin` rule
        - Need to jump to the same element to apply

--- src/features/videosByDuration/VideosByDuration.js
+++ src/features/videosByDuration/VideosByDuration.js
@@ -6,8 +6,8 @@ import VideoLink from '../videoLink/VideoLink';
 export default function VideosByDuration(props) {
 	const durationList = useSelector(getVideosByDuration);
 	return (
-		<section id="duration">
-			<h2>Videos by duration</h2>
+		<section>
+			<h2 id="duration">Videos by duration</h2>
 			{durationList.map(({ video }, i) => (
 				<VideoLink
 					classes="flex gap-2r first-child-9ch"
